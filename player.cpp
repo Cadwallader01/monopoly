b4c22e0bad02daf9fcc2bb3e5f83062ff77ba3d9
Cadwallader01 <rickramos24@gmail.com>
2017-07-07 09:59:02 -0500

    player.cpp

---
+++ player.cpp
@@ -0,0 +1,55 @@
+#include "player.h"
+#include <string>
+#include <iostream>
+
+player::player()
+{
+	name = "player";
+	money = 1500;
+	utilityCount = 0;
+	railroadCount = 0;
+	jailTime = 0;
+}
+player::~player()
+{
+}
+void player::setName(string n)
+{
+	name = n;
+}
+string player::getName()
+{
+	return name;
+}
+void player::setMoney(int m)
+{
+	money = m;
+}
+int player::getMoney()
+{
+	return money;
+}
+void player::setutilityCount(int u)
+{
+	utilityCount = u;
+}
+int player::getutilityCount()
+{
+	return utilityCount;
+}
+void player::setrailroadCount(int r)
+{
+	railroadCount = r;
+}
+int player::getrailroadCount()
+{
+	return railroadCount;
+}
+void player::setjailTime(int j)
+{
+	jailTime = j;
+}
+int player::getjailTime()
+{
+	return jailTime;
+}
