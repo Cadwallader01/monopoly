eea7d446d69e6b747cccecf1c0761c4c29c12cc7
Cadwallader01 <rickramos24@gmail.com>
2017-07-07 22:04:55 -0500

    gameBoard.h

---
+++ gameBoard.h
@@ -0,0 +1,37 @@
+#ifndef GAMEBOARD_H_
+#define GAMEBOARD_H_
+#include <string>
+using namespace std;
+
+class gameBoard
+{
+public:
+	gameBoard();
+	gameBoard(string, int, int, bool);
+	~gameBoard();
+
+
+	void setSquareName(string);
+	string getSquareName();
+
+	//void setHouse(int);
+	//int getHouse();
+	void setSquareCost(int);
+	int getSquareCost();
+	
+	void setSquareRent(int);
+	int getSquareRent();
+
+	void setSquareOwned(bool);
+	bool getSquareOwned();
+
+private:
+	string squareName;
+	int squareCost;
+	//int house;
+	int squareRent;
+	bool squareOwned;
+
+};
+
+#endif
