9922500f10333fd497c205797cf5f72abfe64086
Cadwallader01 <rickramos24@gmail.com>
2017-07-14 22:07:46 -0500

    added ownedBy function to track owner

--- gameBoard.cpp
+++ gameBoard.cpp
@@ -7,21 +7,32 @@ gameBoard *squares = new gameBoard[numOfSquares];
 
 gameBoard::gameBoard()
 {
+	int monopoly = 0;
 	string squareName = "NULL";
 	int squareCost = 0;
 	//int house;
 	int squareRent = 0;
 	bool squareOwned = false;
 }
-gameBoard::gameBoard(string n, int c, int r, bool o)
+gameBoard::gameBoard(int m, string n, int c, int r, bool o, int oB)
 {
+	monopoly = m;
 	squareName = n;
 	squareCost = c;
 	squareRent = r;
 	squareOwned = o;
+	ownedBy = oB;
 }
 gameBoard::~gameBoard()
 {
+}
+void gameBoard::setMonopoly(int m)
+{
+	monopoly = m;
+}
+int gameBoard::getMonopoly()
+{
+	return monopoly;
 }
 void gameBoard::setSquareName(string s)
 {
@@ -56,4 +67,12 @@ void gameBoard::setSquareOwned(bool o)
 bool gameBoard::getSquareOwned()
 {
 	return squareOwned;
+}
+void gameBoard::setOwnedBy(int oB)
+{
+	ownedBy = oB;
+}
+int gameBoard::getOwnedBy()
+{
+	return ownedBy;
 }
