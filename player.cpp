9dc4d87b6c670b39573a4495b6ace2660f4e2ecc
Cadwallader01 <rickramos24@gmail.com>
2017-07-24 21:16:00 -0500

    made player a struct

--- player.cpp
+++ player.cpp
@@ -1,12 +1,14 @@
 #include "player.h"
-#include <string>
+#include <vector>
 #include <iostream>
+using namespace std;
 
+//playerFunctions
 player::player()
 {
 	position = 0;
-	token = 0;
-	name = "player";
+	token = -1;
+	name = "PLAYER";
 	money = 1500;
 	utilityCount = 0;
 	railroadCount = 0;
@@ -21,9 +23,6 @@ player::player(int p, int t, string n, int m, int u, int rail, int jail)
 	utilityCount = u;
 	railroadCount = rail;
 	jailTime = jail;
-}
-player::~player()
-{
 }
 void player::setPosition(int p)
 {
@@ -85,4 +84,38 @@ void player::setjailTime(int j)
 int player::getjailTime()
 {
 	return jailTime;
+}
+
+//function to make a player
+void makePlayer(vector<player>& players)
+{
+	int playerPosition = 0;
+	int playerToken = 0;
+	int amountOfPlayers = 0;
+	string name;
+	cout << "How many players will be playing 1-2?: ";
+	cin >> amountOfPlayers;
+	for (int i = 0; i < amountOfPlayers; i++)
+	{
+		cout << "What is name of player " << i+1 << "? ";
+		cin >> name;
+		players.push_back(player(playerPosition, playerToken, name, 1500, 0, 0, 0));
+		playerToken++;
+	}
+}
+//display players
+void displayPlayer(vector<player>& players)
+{
+	unsigned int size = players.size();
+	for (unsigned int i = 0; i < size; i++)
+	{
+		//cout << players[i].getPosition() << endl;
+		//cout << players[i].getToken() << endl;
+		cout << players[i].getName() << endl;
+		cout << "Starting with: $" << players[0].getMoney() << endl << endl;
+		//cout << players[i].getMoney() << endl;
+		//cout << players[i].getutilityCount() << endl;
+		//cout << players[i].getrailroadCount() << endl;
+		//cout << players[i].getjailTime() << endl;
+	}
 }
