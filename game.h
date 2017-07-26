5f8b537840faf668a0105e14a6da2d8dc9e3c217
Cadwallader01 <rickramos24@gmail.com>
2017-07-25 21:49:42 -0500

    added playerPosition variable to CommunityCards struct

--- game.h
+++ game.h
@@ -41,15 +41,19 @@ struct CommunityCards
 	string card;
 	int moneyOwed;
 	int moneyPaid;
+	int playerPosition;
 
 	CommunityCards();
-	CommunityCards(string, int, int);
+	CommunityCards(string, int, int, int);
 
 	void setCard(string);
 	string getCard();
 
 	int getMoneyOwed();
 	int getMoneyPaid();
+
+	void setPlayerPosition(int p);
+	int getPlayerPosition();
 };
 struct chanceCards
 {
