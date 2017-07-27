d77b25676e45bb47c84a195284e50d7607be9bff
Cadwallader01 <rickramos24@gmail.com>
2017-07-26 21:14:42 -0500

    if statement to check if player is in jail before rolling

--- main.cpp
+++ main.cpp
@@ -8,48 +8,43 @@
 #include <thread>	//sleep_for sleep_until
 using namespace std;
 
-//prototypes for functions
-void pauseTime();
-int diceRoll();
+//PROTOTYPES
 //player functions
-void makePurchase(int, int, vector<property>&, vector<player>&);
 void makePlayer(vector<player>&);
 void displayPlayer(vector<player>&);
-//movement functions
-int makeMove(int, int, vector<player>&, vector<property>&);
 string findOwner(int, vector<player>&);
+void checkJailStatus(int, vector<player>& players);
+bool checkMonopoly(int, int, vector<player>&, vector<property>&);
+void makePurchase(int, int, vector<property>&, vector<player>&);
+
+//game functions
+void pauseTime(int);
+int diceRoll();
+int makeMove(int, int, vector<player>&, vector<property>&);
+void otherSpaces(int, int, vector<player>&, vector<property>&, vector<CommunityCards>&, vector<chanceCards>&);
+void makeBoard(vector<property>&);
+void displayBoard(vector<property>&);
+
 //communityCard functions
 void makeCommunityCards(vector<CommunityCards>&);
 void pickCommunityChestCard(int, vector<player>&, vector<CommunityCards>&);
-void displayCommunityCards(vector<CommunityCards>&);
+
 //chanceCard functions
 void makeChanceCards(vector<chanceCards>&);
 void pickChanceCards(int, vector<player>&, vector<chanceCards>&);
-//board functions
-void makeBoard(vector<property>&);
-void makeCommunityCards(vector<CommunityCards>&);
-bool checkMonopoly(int, int, vector<player>&, vector<property>&);
-void otherSpaces(int, int, vector<player>&, vector<property>&, vector<CommunityCards>&, vector<chanceCards>&);
-void displayBoard(vector<property>&);
 
 int main(){
 
-	//WORKING ON: building otherSpaces function
-
-	//NOTES: When player wants to build a house checkMonoply will check where they can build houses
-	//NOTES: Check if made a monopoly with last purchase or if user selects option to build
-	//			-work on functionality to build houses/hotels
-	//NOTES: Work on Community Cards
-	//			-work on jail cards?
-	//			-work on cards that make you move places
-	//			-work on cards that put you in jail
+	//WORKING ON: pickCommunityChestCard
 	//			-work on cards that give you money from other players
 	//			-work on cards that make you pay players
 	//			-work on cards that make you fix houses/hotels
+
+	//NOTES: When player wants to build a house checkMonoply will check where they can build houses
+	//			-work on functionality to build houses/hotels
 	//NOTES: Work on Chance Cards
 	//NOTES: Before game starts ALLOW USERS TO ROLL DICE TO SEE WHO GOES FIRST
 	//NOTES: Buying - make sure player has enough money to buy---otherwise can't afford property
-	//NOTES: Change items to refer to "properties" vs squares/boardSquares?-doesn't seem valid for jail, community, chance
 
 
 	string userInput;
@@ -68,18 +63,20 @@ int main(){
 	cout << "Welcome to monopoly!" << endl;
 	//initialize players
 	makePlayer(players);
-	cout << endl << endl << "Current board:" << endl;
+	//display player information
+	cout << endl;
+	displayPlayer(players);
+	pauseTime(3);
+	cout << endl << endl << "Current Monopoly Board" << endl;
 	//initialize and display board
 	makeBoard(property);
 	displayBoard(property);
+	cout << endl;
+	pauseTime(5);
 	//initialize community cards
 	makeCommunityCards(communityCard);
 	//initialize chance cards
 	makeChanceCards(chanceCard);
-
-	//display player information
-	cout << endl;
-	displayPlayer(players);
 
 	//loop to play the game
 	while(players[currentPlayer].getMoney() > 0)
@@ -104,21 +101,31 @@ int main(){
 		}
 
 		cout << endl << endl << endl << "----------------------------------TURN----------------------------" << endl;
-		cout << "It's " << players[currentPlayer].getName() << "'s turn! Roll the dice!" << endl;
-		pauseTime();
-		//**********************************ROLLING THE DICE************************************
-		int roll = diceRoll();
-		cout << "You rolled a..." << roll << endl;
-		//sets player position-coming back around the board if necessary
-		roll = makeMove(roll, currentPlayer, players, property);
-		players[currentPlayer].setPosition(roll);
+		//********************CHECK IF PLAYER IS IN JAIL BEFORE THEY CAN MOVE*******************
+		if (players[currentPlayer].getjailTime() == false)
+		{
+			//**********************************ROLLING THE DICE************************************
+			cout << "It's " << players[currentPlayer].getName() << "'s turn! Roll the dice!" << endl;
+			pauseTime(5);	//pause for 5 seconds
+			//**********************************ROLLING THE DICE************************************
+			int roll = diceRoll();
+			cout << "You rolled a " << roll << endl;
+			pauseTime(5);	//pause for 5 seconds
+			//sets player position-coming back around the board if necessary
+			roll = makeMove(roll, currentPlayer, players, property);
+			players[currentPlayer].setPosition(roll);
 		
-		//******************************MAKING/NOT MAKING A PURCHASE****************************
-		cout << "You landed on: " << property[roll].getPropertyName() << endl;
-		//check if player can purchase square or if square is owned
-		makePurchase(roll, currentPlayer, property, players);
-		//check if square is Go, Community Chest, or Chance and do necessary...
-		otherSpaces(roll, currentPlayer, players, property, communityCard, chanceCard);
+			//******************************MAKING/NOT MAKING A PURCHASE****************************
+			cout << "You landed on: " << property[roll].getPropertyName() << endl;
+			//check if player can purchase square or if square is owned
+			makePurchase(roll, currentPlayer, property, players);
+			//check if square is Go, Community Chest, or Chance and do necessary...
+			otherSpaces(roll, currentPlayer, players, property, communityCard, chanceCard);
+		}
+		else if (players[currentPlayer].getjailTime() == true)
+		{
+			checkJailStatus(currentPlayer, players);
+		}
 		//**************************************OTHER OPTIONS***********************************
 		cout << "Would you like to do anything else? (Type 'b' for build or 'n' for No (NOTE: Trading Unavailable) "<<endl; 
 		cin >> userInput;
