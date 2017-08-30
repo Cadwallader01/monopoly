7c8d3c9bd2abbb69a3012fc598503a6de2703a85
Cadwallader01 <rickramos24@gmail.com>
2017-08-29 22:13:37 -0500

    newest version of game.h file

--- game.h
+++ game.h
@@ -3,26 +3,36 @@
 #include <string>
 using namespace std;
 
-struct property{
-	int monopoly;
+struct property
+{
+	int monopolyGroup;
+	bool monopoly;
 	string squareName;
 	int squareCost;
-	//int house;
+	int house;
+	int hotel;
 	int squareRent;
 	bool squareOwned;
 	int ownedBy;
 	//functions
 	property();
-	property(int, string, int, int, bool, int);
+	property(int, bool, string, int, int, int, int, bool, int);
 
-	void setMonopoly(int);
-	int getMonopoly();
+	void setMonopolyGroup(int);
+	int getMonopolyGroup();
+
+	void setMonopoly(bool);
+	bool getMonopoly();
 
 	void setPropertyName(string);
 	string getPropertyName();
 
-	//void setHouse(int);
-	//int getHouse();
+	void setHouse(int);
+	int getHouse();
+
+	void setHotel(int);
+	int getHotel();
+
 	void setPropertyCost(int);
 	int getPropertyCost();
 	
@@ -35,40 +45,4 @@ struct property{
 	void setOwnedBy(int);
 	int getOwnedBy();
 };
-
-struct CommunityCards
-{
-	string card;
-	int moneyOwed;
-	int moneyPaid;
-	int playerPosition;
-
-	CommunityCards();
-	CommunityCards(string, int, int, int);
-
-	void setCard(string);
-	string getCard();
-
-	int getMoneyOwed();
-	int getMoneyPaid();
-
-	void setPlayerPosition(int p);
-	int getPlayerPosition();
-};
-struct chanceCards
-{
-	string card;
-	int moneyOwed;
-	int moneyPaid;
-
-	chanceCards();
-	chanceCards(string, int, int);
-
-	void setCard(string);
-	string getCard();
-
-	int getMoneyOwed();
-	int getMoneyPaid();
-};
-
 #endif
